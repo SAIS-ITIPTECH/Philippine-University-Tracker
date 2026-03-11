b1c470ac02f1767197975287e43b8e40e61a1738
Lourence L. <lorejaslourence@gmail.com>
2026-03-11 09:42:45 +0800

    Fix script loading and improve form submission

--- Contact-Us/script.js
+++ Contact-Us/script.js
@@ -1,30 +1,31 @@
-// 1. Initialize EmailJS
-emailjs.init({publicKey: "grh1BJmsO_p19oKHs"});
 
-// 2. Listen for the submit
-const myForm = document.getElementById("contactForm");
+document.addEventListener("DOMContentLoaded", function (){
+    
+    emailjs.init({publicKey: "grh1BJmsO_p19oKHs"})
 
-if (myForm) {
-    myForm.addEventListener("submit", function(event) {
-        // 3. STOP the page from refreshing/changing the URL
-        event.preventDefault();
+    const myForm = document.getElementById("contactForm");
 
-        const templateParams = {
-            name: document.getElementById("name").value,
-            email: document.getElementById("email").value,
-            subject: document.getElementById("subject").value,
-            message: document.getElementById("message").value
-        };
+    if (myForm) {
+        myForm.addEventListener("submit", function(event) {
+            event.preventDefault();
 
-        // 4. Send the email
-        emailjs.send("service_lfbii0h", "template_9vm5tmv", templateParams)
-            .then(function() {
-                alert("Sent successfully!");
-                myForm.reset();
-            }, function(error) {
-                alert("Error: " + JSON.stringify(error));
-            });
-    });
-} else {
-    console.error("Could not find the form! Check your ID.");
-}
+            const templateParams = {
+                name: document.getElementById("name").value,
+                email: document.getElementById("email").value,
+                subject: document.getElementById("subject").value,
+                message: document.getElementById("message").value
+            };
+
+            emailjs.send("service_lfbii0h", "template_9vm5tmv", templateParams)
+                .then(function() {
+                    alert("Sent successfully!");
+                    myForm.reset();
+                }, function(error) {
+                    alert("Error: " + JSON.stringify(error));
+                });
+        });
+    } else {
+        console.error("Could not find the form! Check your ID.");
+    }
+
+});
